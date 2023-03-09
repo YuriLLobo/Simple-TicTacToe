2ff0ab79f13b1bd8ff23f833ab8697c79ca6d0b0
Yuri Lobo <yurilobodev@gmail.com>
2023-03-09 10:01:56 -0300

    Adição do HTML, CSS e JS básico do projeto

---
+++ index.js
@@ -0,0 +1,45 @@
+const squares = document.querySelectorAll('button');
+let currentPlayer = 'X';
+let gameFinished = false;
+
+function playTurn() {
+  if (gameFinished) return;
+
+  const square = this;
+  if (square.textContent !== '') return;
+
+  square.textContent = currentPlayer;
+
+  if (checkWin()) {
+    alert(`Player ${currentPlayer} won!`);
+    gameFinished = true;
+    return;
+  }
+
+  currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+}
+
+function checkWin() {
+  const winningCombos = [
+    [0, 1, 2], [3, 4, 5], [6, 7, 8], // horizontal
+    [0, 3, 6], [1, 4, 7], [2, 5, 8], // vertical
+    [0, 4, 8], [2, 4, 6] // diagonal
+  ];
+
+  for (let i = 0; i < winningCombos.length; i++) {
+    const combo = winningCombos[i];
+    if (
+      squares[combo[0]].textContent === currentPlayer &&
+      squares[combo[1]].textContent === currentPlayer &&
+      squares[combo[2]].textContent === currentPlayer
+    ) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+squares.forEach(square => {
+  square.addEventListener('click', playTurn);
+});
